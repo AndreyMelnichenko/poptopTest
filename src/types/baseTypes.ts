d328ffbe8c671a07cfab6db50ddb374d6fc52e82
Andrii Melnychenko <andrey.melnychenko@px.com>
2024-07-12 19:53:07 +0300

    add new changes

---
+++ src/types/baseTypes.ts
@@ -0,0 +1,6 @@
+export class BaseTypes {
+
+  isEqual(a: number, b: number): string {
+    return `${a} and ${b}`;
+  }
+}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { BaseTypes } from "../src/types/baseTypes";
 
 test("has title", async ({ page }) => {
   await page.goto("/");
@@ -12,4 +13,25 @@ test("has search page", async ({ page }) => {
   await page.getByTestId("Button").getByText("SEARCH").click();
   await page.waitForURL("**/search/?");
   await expect(page.locator("div[data-scrollbar]"), "Search page didn't opened").toBeVisible({ timeout: 10000 });
+
+});
+
+test("Click on View All", async ({ page }) => {
+  await page.goto("/");
+  await page.getByTestId("Stack").locator("[href='/search/?category=catering']").click();
+});
+
+test("Click on Picture", async ({ page }) => {
+  await page.goto("/");
+  await page
+    .locator("h4")
+    .getByText("BBQ")
+    .locator(" ~ img")
+    .first()
+    .click();
+});
+
+test.only("Check values", async () => {
+  const mytypes = new BaseTypes();
+  console.log(mytypes.isEqual(444, 3454545));
 });
